5d3f33d3603157996ca9da95e3378b4174cd70ae
wenhua0902 <s1100396@gm.pu.edu.tw>
2022-12-23 15:15:59 +0800

    version2

--- index.py
+++ index.py
@@ -16,6 +16,7 @@ def index():
     homepage += "<a href=/account>網頁表單輸入實例</a><br>"
     homepage += "<a href=/I>黃汶樺簡介網頁</a><br>"
     homepage += "<a href=/search>食物查詢</a><br>"
+    homepage += "<a href=/webhook>查詢</a><br>"
     return homepage
 
 
@@ -72,6 +73,36 @@ def search():
         return result
     else:
         return render_template("search.html")
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    #if (action == "food"):
+        # Cond =  req.get("queryResult").get("parameters").get("location")
+        # keyword =  req.get("queryResult").get("parameters").get("time")
+        # info = "您要查詢地點的" + Cond + "，關鍵字是：" + keyword + "\n\n"
+    #     #if (Cond == "food"):
+    #         collection_ref = db.collection("食物")
+    #         docs = collection_ref.get()
+    #         found = False
+    #         for doc in docs:
+    #             if keyword in doc.to_dict()["food"]:
+    #                 found = True 
+    #                 info += "地點：" + doc.to_dict()["地點"] + "\n\n" 
+    #                 info += "時段：" + doc.to_dict()["時段"] + "\n\n" 
+    #                 info += "店家名稱：" + doc.to_dict()["店家名稱"] + "\n\n" 
+    #                 info += "地址：" + doc.to_dict()["地址"] + "\n\n"
+    #                 info += "營業時間：" + doc.to_dict()["營業時間"] + "\n\n"
+    #                 info += "評價：" + doc.to_dict()["評價"] + "\n\n" 
+    #                 info += "類型：" + doc.to_dict()["類型"] + "\n"
+    #         if not found:
+    #             info += "很抱歉，目前無符合這個關鍵字的相關食物喔"  
+    # return make_response(jsonify({"fulfillmentText": info}))
 
 
 if __name__ == "__main__":
